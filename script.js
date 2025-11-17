0f695e77ed100ae797477a80711ae5e5b8b1b9a8
nnasirlone1-collab <nnasir.lone1@gmail.com>
2025-11-17 20:48:01 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let coins = localStorage.getItem("coins") ? Number(localStorage.getItem("coins")) : 0;
-
-document.getElementById("coins").innerText = "Coins: " + coins;
-
-function mine() {
-  coins += 1;
-  document.getElementById("coins").innerText = "Coins: " + coins;
-  localStorage.setItem("coins", coins);
-}
